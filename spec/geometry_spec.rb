c4cf34766d69771a56234e5ada5cb8a2861186c4
Vamshi Palreddy <vamshi29292@gmail.com>
2016-09-29 13:20:23 +0530

    Added a test to verify that points are read only:

--- spec/geometry_spec.rb
+++ spec/geometry_spec.rb
@@ -7,5 +7,17 @@ describe Geometry::Point do
       expect(point.x).to eq(4)
       expect(point.y).to eq(5)
     end
+
+    it "a point should not be writable and raises NoMethodError" do
+      point = Geometry::Point.new(3,2)
+      expect{
+        point.x = 4
+      }.to raise_error(NoMethodError)
+      expect{
+        point.y = 4
+      }.to raise_error(NoMethodError)
+      expect(point.x).to eq(3)
+      expect(point.y).to eq(2)
+    end
   end
 end
