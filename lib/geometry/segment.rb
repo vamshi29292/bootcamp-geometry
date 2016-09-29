d9f730ff7be52ed5e184d4aa455ca932c63fe7a6
Vamshi Palreddy <vamshi29292@gmail.com>
2016-09-29 15:56:49 +0530

    Add Segment#length method to calculate the length of line segment and write specs to test the length

--- lib/geometry/segment.rb
+++ lib/geometry/segment.rb
@@ -5,4 +5,9 @@ class Geometry::Segment
     @end1 = Geometry::Point.new(x1,y1)
     @end2 = Geometry::Point.new(x2,y2)
   end
+
+  def length
+    square_of_length = (end1.x_coord - end2.x_coord)**2 + (end1.y_coord - end2.y_coord)**2
+    Math.sqrt(square_of_length)
+  end
 end
--- spec/segment/segment_spec.rb
+++ spec/segment/segment_spec.rb
@@ -1,11 +1,23 @@
 describe Geometry::Segment do
   describe "#initialize" do
-    it "should create a new segment with two ends when given 4 co-ordinates" do |variable|
+    it "should create a new segment with two ends when given 4 co-ordinates" do
       segment = Geometry::Segment.new(4,1,2,3)
       expect(segment.end1.x_coord).to eq(4)
       expect(segment.end1.y_coord).to eq(1)
       expect(segment.end2.x_coord).to eq(2)
       expect(segment.end2.y_coord).to eq(3)
+    end
+  end
+
+  describe "length" do
+    it "should return 4 when given 0,0,4,0" do
+      segment = Geometry::Segment.new(0,0,4,0)
+      expect(segment.length).to eq(4)
+    end
+
+    it "should return 5 when given 0,0,3,4" do
+      segment = Geometry::Segment.new(0,0,3,4)
+      expect(segment.length).to eq(5)
     end
   end
 end
