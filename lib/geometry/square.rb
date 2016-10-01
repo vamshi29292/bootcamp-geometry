fb8318498fd3e133dc849f1f50bd247aee21212a
Vamshi Palreddy <vamshi29292@gmail.com>
2016-10-01 16:36:12 +0530

    Modify Square class to inherit from Rectangle class

--- lib/geometry/square.rb
+++ lib/geometry/square.rb
@@ -1,21 +1,5 @@
-class Geometry::Square
-  attr_reader :side
-
+class Geometry::Square < Geometry::Rectangle
   def initialize(side)
-    @side = side
-  end
-
-  def perimeter
-    return Float::INFINITY unless valid?
-    4*side
-  end
-
-  def area
-    return Float::INFINITY unless valid?
-    side**2
-  end
-
-  def valid?
-    side >= 0
+    super(side, side)
   end
 end
